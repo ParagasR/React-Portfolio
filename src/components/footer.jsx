9bea9cafb2a66d903a70812d9ccfae5100489759
paragasR <r.paragas@outlook.com>
2022-03-13 20:49:31 -0600

    fixed build error

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const mainExport = () => {
   return (
     <footer className="footer content m-0 p-4">
       <div>
-        <img src={} />
+        {/* <img src={} /> */}
         <img />
         <img />
       </div>
